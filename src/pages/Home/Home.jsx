8b0f6e51badb76c227f62dd9e194894c39203273
brunoascenc <brunoascenc1@outlook.com>
2021-08-17 16:15:22 -0300

    home page component

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopChart from '../../components/TopChart/TopChart';
+
+const Home = () => {
+  return (
+    <div>
+      <TopChart />
+    </div>
+  );
+};
+
+export default Home;
