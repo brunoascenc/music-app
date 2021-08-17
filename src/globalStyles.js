695098187a76e29c939a3b2610af20b826b702c4
brunoascenc <brunoascenc1@outlook.com>
2021-08-17 16:16:05 -0300

    styled components global css

---
+++ src/globalStyles.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+ * {
+   margin: 0;
+   padding: 0;
+   box-sizing: border-box;
+  }
+
+  *:focus {
+   outline: none;
+  }
+
+ a{
+   text-decoration: none;
+  }
+
+  body{
+    overflow-x: hidden;
+  }
+  ul{
+    list-style: none;
+  }
+`;
+
+export default GlobalStyle;
