63cfc4a700f899807e3e65c9f7abd889b16b2403
brunoascenc <brunoascenc1@outlook.com>
2021-08-18 22:07:43 -0300

    font family

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import Search from './pages/Search/Search';
 import Home from './pages/Home/Home';
 import Favorite from './pages/Favorite/Favorite';
+import SideMenu from './components/SideMenu/SideMenu';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Header />
+      <SideMenu />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/search=:pathname?" component={Search} />
--- src/globalStyles.js
+++ src/globalStyles.js
@@ -13,13 +13,28 @@ const GlobalStyle = createGlobalStyle`
 
  a{
    text-decoration: none;
+   color: #332f2f;
   }
 
   body{
     overflow-x: hidden;
+    font-family: 'Poppins', sans-serif;
+    background-color: #ffffff;
+    color: #332f2f;
   }
   ul{
     list-style: none;
+  }
+
+  .container{
+    padding-left: 5%;
+    padding-right: 5%;
+    margin-left: 15vw;
+  }
+
+  .section-title{
+    margin-bottom: 20px;
+    margin-top: 70px;
   }
 `;
 
