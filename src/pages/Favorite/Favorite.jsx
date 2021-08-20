c75d20e1849433208624a26585f15563be7a08ea
brunoascenc <brunoascenc1@outlook.com>
2021-08-19 23:52:50 -0300

    changing css file

--- src/pages/Favorite/Favorite.jsx
+++ src/pages/Favorite/Favorite.jsx
@@ -2,63 +2,60 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeTrack } from '../../redux/favorites/favorites-actions';
 import Deezer from '../../images/deezer-logo.png';
-import { AiOutlineDelete } from 'react-icons/ai';
+import { BsFillPlayFill } from 'react-icons/bs';
+import { AiFillDelete } from 'react-icons/ai';
 import {
   List,
   ListItem,
   Cover,
   TrackInfo,
   ActionLinks,
-} from './FavoriteStyles';
+  PlayButton,
+  Span,
+} from '../../components/Tracks/TracksStyles';
+import { playTrack } from '../../redux/player/player-actions';
+import useTimeConvert from '../../hooks/useTimeConvert.js';
 
 const Favorite = () => {
   const favorites = useSelector((state) => state.favorites.trackList);
   const dispatch = useDispatch();
-
-  function timeConvert(time) {
-    let hrs = ~~(time / 3600);
-    let mins = ~~((time % 3600) / 60);
-    let secs = ~~time % 60;
-
-    let ret = '';
-    if (hrs > 0) {
-      ret += '' + hrs + ':' + (mins < 10 ? '0' : '');
-    }
-    ret += '' + mins + ':' + (secs < 10 ? '0' : '');
-    ret += '' + secs;
-    return ret;
-  }
+  const [timeConvert] = useTimeConvert();
 
   return (
     <div className="container">
-      <h2 className="section-title">Minha playlist</h2>
+      <h2 className="section-title">Músicas Favoritas</h2>
       <List>
         {favorites &&
           favorites.map((track) => {
             return (
               <ListItem key={track.id}>
-                <Cover>
-                  <img src={track.album.cover_medium} alt={track.album.title} />
+                <Cover onClick={() => dispatch(playTrack(track.preview))}>
+                  <div>
+                    <img
+                      src={track.album.cover_medium}
+                      alt={track.album.title}
+                    />
+                    <PlayButton>
+                      <BsFillPlayFill />
+                    </PlayButton>
+                  </div>
+                  <TrackInfo>
+                    <Span titleTrack>{track.title}</Span>
+                    <Span>{track.artist.name}</Span>
+                  </TrackInfo>
                 </Cover>
-                <TrackInfo>
-                  <span className="track-title">{track.title}</span>
-                  <span>{track.artist.name}</span>
-                </TrackInfo>
 
                 <ActionLinks>
                   <div>
-                    <span>{timeConvert(track.duration)}</span>
+                    <Span>{timeConvert(track.duration)}</Span>
                     <button onClick={() => dispatch(removeTrack(track))}>
-                      <AiOutlineDelete />
+                      <AiFillDelete />
                     </button>
                   </div>
                   <a className="deezer-link" href={track.link}>
                     <img src={Deezer} alt={track.link} />
                   </a>
                 </ActionLinks>
-                {/* <audio controls>
-                  <source src={track.preview} type="audio/mpeg" />
-                </audio> */}
               </ListItem>
             );
           })}
