c0087483cf521e78960f3711e95af29caa547e3f
brunoascenc <brunoascenc1@outlook.com>
2021-08-20 14:34:13 -0300

    alerta em caso de duplicidade

--- src/redux/favorites/favorites-utils.js
+++ src/redux/favorites/favorites-utils.js
@@ -1,9 +1,11 @@
 export const addTrackToList = (trackList, trackToAdd) => {
   const existingTrack = trackList.find((track) => track.id === trackToAdd.id);
-  alert('Música salva');
 
   if (existingTrack) {
+    alert('Música já está na sua lista');
     return trackList;
+  } else {
+    alert('Música salva!');
   }
 
   return [...trackList, { ...trackToAdd }];
