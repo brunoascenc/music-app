d9470d212f741356db239fc745d6c716780fe3fe
brunoascenc <brunoascenc1@outlook.com>
2021-08-19 23:53:07 -0300

    pulling data from redux

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import TopChart from '../../components/TopChart/TopChart';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchTopChartStart } from '../../redux/chart/chart-actions';
+import Tracks from '../../components/Tracks/Tracks';
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const chart = useSelector((state) => state.chart.results);
+  const loading = useSelector((state) => state.chart.loading);
+  const [pageNumber, setPageNumber] = useState(30);
+
+  useEffect(() => {
+    dispatch(fetchTopChartStart(pageNumber));
+  }, [dispatch, pageNumber]);
+
   return (
     <div className="container">
-      <TopChart />
+      <h2 className="section-title">Top Charts</h2>
+      <Tracks pageNumber={setPageNumber} loading={loading} data={chart} />
     </div>
   );
 };
