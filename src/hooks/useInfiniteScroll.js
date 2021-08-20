216611ce7d5851f94ef90a69bd74c68c6c814cd3
brunoascenc <brunoascenc1@outlook.com>
2021-08-19 23:49:38 -0300

    custom hooks

---
+++ src/hooks/useInfiniteScroll.js
@@ -0,0 +1,22 @@
+import { useRef, useCallback } from 'react';
+
+const useInfiniteScroll = (loading, pageNumber) => {
+  const observer = useRef();
+  const lastTrackElement = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          pageNumber((prevPageNumber) => prevPageNumber + 10);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, pageNumber]
+  );
+
+  return [lastTrackElement];
+};
+
+export default useInfiniteScroll;
