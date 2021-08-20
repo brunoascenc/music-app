4293a00db9813e68f164ea650058cf19cefc7567
brunoascenc <brunoascenc1@outlook.com>
2021-08-19 23:48:03 -0300

    spinner component

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SpinnerContainer, SpinnerOverlay } from './SpinnerStyles';
+
+const Spinner = () => {
+  return (
+    <SpinnerContainer>
+      <SpinnerOverlay />
+    </SpinnerContainer>
+  );
+};
+
+export default Spinner;
